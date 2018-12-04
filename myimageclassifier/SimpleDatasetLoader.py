0fe83268c6fd5407da957d0e36b00e3a20131656
Saeed Arbabi <gsarbabi@gmail.com>
2018-12-04 10:05:35 +0330

    Add files via upload

---
+++ myimageclassifier/SimpleDatasetLoader.py
@@ -0,0 +1,8 @@
+
+class SimpleDatasetLoader:
+    def __init__(self, preprocessors):
+    	pass
+
+    def load(self, imagePaths, verbose=-1):
+        pass
+
---
+++ myimageclassifier/SimplePreprocessor.py
@@ -0,0 +1,8 @@
+
+
+class SimplePreprocessor:
+    def __init__(self, width, height):
+        pass
+
+    def preprocess(self, image):
+        pass
